1e9e1a7182d1c3f055eae32d0f467520bb933404
Vinay <imvinaytajane@gmail.com>
2022-02-20 11:47:38 +0530

    3d graphs

---
+++ surface_triangulation.py
@@ -0,0 +1,17 @@
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def f(x, y):
+    return np.sin(np.sqrt(x**2+y**2))
+
+
+theta = 2*np.pi*np.random.random(1000)
+r = 6*np.random.random(1000)
+x = np.ravel(r*np.sin(theta))
+y = np.ravel(r*np.cos(theta))
+z = f(x, y)
+ax = plt.axes(projection='3d')
+ax.plot_trisurf(x, y, z, cmap='viridis', edgecolor='none')
+plt.show()
